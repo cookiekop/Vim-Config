548a936b918e8b96dbe242cb2514a841453db1be
cookiekop <michaelzhang1234@gmail.com>
2021-09-24 12:15:42 +0800

    add config script.

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "Install vim configurations..."
+
+echo "backup vim config"
+
+if [ -f "~/.vimrc" ]; then
+    mv ~/.vimrc ~/.vimrc.'bak'
+fi
+
+if [ -d "~/.vim" ]; then
+    mv ~/.vim ~/.vim.`bak`
+fi
+
+echo "create new vimrc..."
+mv ../.vim ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+
+vi +PlugInstall! +qall
